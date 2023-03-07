ec94dc7f34b4b750172f87f58f67d07c5caa0278
Tolga Özgün <tolgaozgunn@gmail.com>
2023-03-08 00:04:09 +0300

    Fix REST API

--- backend/src/main/java/edu/bikent/achilles/AchillesApplication.java
+++ backend/src/main/java/edu/bikent/achilles/AchillesApplication.java
@@ -1,9 +1,13 @@
 package edu.bikent.achilles;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @SpringBootApplication
+@RestController
 public class AchillesApplication {
 
 	public static void main(String[] args) {
