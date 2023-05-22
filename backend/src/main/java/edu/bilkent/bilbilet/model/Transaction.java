14a17a67dd99d3401936c6bb0bab839034c8aae5
Yagiz Can Aslan <can.aslan@ug.bilkent.edu.tr>
2023-05-22 21:23:47 +0300

    Transaction model

---
+++ backend/src/main/java/edu/bilkent/bilbilet/model/Transaction.java
@@ -0,0 +1,28 @@
+package edu.bilkent.bilbilet.model;
+
+import lombok.AllArgsConstructor;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Transaction {
+    @Id
+    private int transaction_id;
+
+    @NotBlank
+    private String transaction_type;
+
+    @NotNull
+    private int transaction_amount;
+
+    @NotNull
+    private int receiver_id;
+
+    @NotNull
+    private int sender_id;
+}
