d1a68fe8e927e7ff8a21b7920e3b395f8a5a9647
CanErsoy20 <can.ersoy@ug.bilkent.edu.tr>
2023-05-27 02:08:57 +0300

    review page init

---
+++ frontend/src/components/review/AddReviewCard.tsx
@@ -0,0 +1,11 @@
+import { Card, Flex } from '@mantine/core';
+
+const AddReviewCard = () => {
+	return (
+		<Card>
+			<Flex direction={'column'} gap={'sm'}></Flex>
+		</Card>
+	);
+};
+
+export default AddReviewCard;
---
+++ frontend/src/pages/review/ReviewPage.tsx
@@ -0,0 +1,12 @@
+import { Center } from '@mantine/core';
+import AddReviewCard from '../../components/review/AddReviewCard';
+
+const ReviewPage = () => {
+	return (
+		<Center>
+			<AddReviewCard></AddReviewCard>
+		</Center>
+	);
+};
+
+export default ReviewPage;
