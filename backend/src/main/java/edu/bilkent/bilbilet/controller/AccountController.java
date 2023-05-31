e06fa737400d8a255b480550393b672d5d486f6f
Tolga Özgün <tolgaozgunn@gmail.com>
2023-05-31 09:36:11 +0300

    Update Company

--- backend/src/main/java/edu/bilkent/bilbilet/controller/AccountController.java
+++ backend/src/main/java/edu/bilkent/bilbilet/controller/AccountController.java
@@ -1,5 +1,6 @@
 package edu.bilkent.bilbilet.controller;
 
+import edu.bilkent.bilbilet.request.CompanyRegister;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -61,11 +62,11 @@ public class AccountController {
     }
 
     @CrossOrigin(origins = "http://localhost:5173", allowedHeaders = "*", allowCredentials = "true")
-    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, path = "register/traveller")
-    public ResponseEntity<Object> registerCompany(@Valid @RequestBody TravelerRegister travelerInfo) {
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, path = "register/company")
+    public ResponseEntity<Object> registerCompany(@Valid @RequestBody CompanyRegister companyRegister) {
         try {
-            TravelerRegister savedInfo = accountService.addTraveler(travelerInfo);
-            return Response.create("Company account registered", HttpStatus.OK, savedInfo.getUser());
+            CompanyRegister savedInfo = accountService.addCompany(companyRegister);
+            return Response.create("Company account registered", HttpStatus.OK, savedInfo.getCompany());
         } catch (Exception e) {
             return Response.create(ExceptionLogger.error(e), HttpStatus.INTERNAL_SERVER_ERROR);
         }
