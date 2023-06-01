f555d34822e3fce75d53e97f5acdb66d601852c5
Ilkim Elif Kervan <elif.kervan.1907@gmail.com>
2023-06-02 00:30:33 +0300

    Added rent service

---
+++ backend/src/main/java/edu/bilkent/bilbilet/service/RentService.java
@@ -0,0 +1,98 @@
+package edu.bilkent.bilbilet.service;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Random;
+import java.util.UUID;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import edu.bilkent.bilbilet.enums.FuelType;
+import edu.bilkent.bilbilet.enums.UserType;
+import edu.bilkent.bilbilet.exception.CarException;
+import edu.bilkent.bilbilet.exception.RentException;
+import edu.bilkent.bilbilet.model.Address;
+import edu.bilkent.bilbilet.model.Car;
+import edu.bilkent.bilbilet.model.CarBrand;
+import edu.bilkent.bilbilet.model.CompanyCar;
+import edu.bilkent.bilbilet.model.RentDetail;
+import edu.bilkent.bilbilet.model.Traveler;
+import edu.bilkent.bilbilet.model.User;
+import edu.bilkent.bilbilet.repository.AccountRepository;
+import edu.bilkent.bilbilet.repository.AddressRepository;
+import edu.bilkent.bilbilet.repository.CarBrandRepository;
+import edu.bilkent.bilbilet.repository.CarRepository;
+import edu.bilkent.bilbilet.repository.CompanyCarRepository;
+import edu.bilkent.bilbilet.repository.RentDetailRepository;
+import edu.bilkent.bilbilet.repository.rowmapper.CompanyCarRM;
+import edu.bilkent.bilbilet.request.AddCompanyCar;
+import edu.bilkent.bilbilet.request.TravelerRegister;
+import edu.bilkent.bilbilet.request.UserLogin;
+import edu.bilkent.bilbilet.response.RRefreshToken;
+import edu.bilkent.bilbilet.response.RUserToken;
+import edu.bilkent.bilbilet.security.JWTFilter;
+import edu.bilkent.bilbilet.security.JWTUserService;
+import edu.bilkent.bilbilet.security.JWTUtils;
+import edu.bilkent.bilbilet.utils.Utils;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class RentService {
+
+    private final CarRepository carRepository;
+    private final CompanyCarRepository companyCarRepository;
+    private final CarBrandRepository carBrandRepository;
+    private final AddressRepository addressRepository;
+    private final RentDetailRepository rentDetailRepository;
+
+    public Car rentCar(RentDetail rd) throws Exception {
+        try {
+            // check if car already exists
+            boolean carExist = companyCarRepository.existByCompanyCarId(rd.getCompanyCarId());
+
+            if (!carExist) {
+                throw new RentException("Car not found");
+            }
+
+            if (rd.getStartDate() > rd.getEndDate()) {
+                throw new RentException("End date cannot be before the start date");
+            }
+
+            Car savedCar = carRepository.save(car);
+            return savedCar;
+        } catch (RentException ce) {
+            throw ce;
+        } catch (Exception e) {
+            System.out.println("Car cannot be added yahu");
+            e.printStackTrace();
+            throw e;
+        }
+    }
+
+    public List<CompanyCarRM> findAvaliableCars(Map<String, Object> requestParams) throws Exception {
+        try {
+            Map<String, Object> snakeParams = Utils.camelToSnake(requestParams);
+
+            boolean startExist = snakeParams.get("start_date") != null;
+            boolean endExist = snakeParams.get("end_date") != null;
+
+            if (startExist != endExist) {
+                throw new RentException("Please provide start date and and date together");
+            }
+
+            return rentDetailRepository.findAvailableCarsByProperties(snakeParams); // TODO test // TODO check when start and end date is the same maybe change data type to date ????  
+        } catch (RentException e) {
+            throw e;
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw e;
+        }
+    }
+}
