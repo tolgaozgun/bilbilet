afbe8ab2f2db05b53f24d1bf3a053a76e6889965
Tolga Özgün <tolgaozgunn@gmail.com>
2023-06-02 16:42:55 +0300

    Add Transaction Changes

--- backend/src/main/java/edu/bilkent/bilbilet/repository/AccountRepository.java
+++ backend/src/main/java/edu/bilkent/bilbilet/repository/AccountRepository.java
@@ -50,9 +50,67 @@ public class AccountRepository {
             return null;
         } catch (Exception e) {
             e.printStackTrace();
+            throw e;
         }
 
-        return null;
+//        return null;
+    }
+
+    public void incrementTravelerBalance(int userId, BigDecimal amount) throws Exception {
+        String sql = "UPDATE Traveler SET balance = balance + ? WHERE user_id = ?";
+
+        try {
+            jdbcTemplate.update(sql, userId, amount);
+        } catch (EmptyResultDataAccessException e) {
+            throw new Exception("Traveler not found");
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw e;
+        }
+
+    }
+
+    public void decrementTravelerBalance(int userId, BigDecimal amount) throws Exception {
+        String sql = "UPDATE Traveler SET balance = balance - ? WHERE user_id = ?";
+
+        try {
+            jdbcTemplate.update(sql, userId, amount);
+        } catch (EmptyResultDataAccessException e) {
+            throw new Exception("Traveler not found");
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw e;
+        }
+
+    }
+
+
+    public void incrementCompanyBalance(int userId, BigDecimal amount) throws Exception {
+        String sql = "UPDATE Company SET balance = balance + ? WHERE user_id = ?";
+
+        try {
+            jdbcTemplate.update(sql, userId, amount);
+        } catch (EmptyResultDataAccessException e) {
+            throw new Exception("Company not found");
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw e;
+        }
+
+    }
+
+    public void decrementCompanyBalance(int userId, BigDecimal amount) throws Exception {
+        String sql = "UPDATE Company SET balance = balance - ? WHERE user_id = ?";
+
+        try {
+            jdbcTemplate.update(sql, userId, amount);
+        } catch (EmptyResultDataAccessException e) {
+            throw new Exception("Company not found");
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw e;
+        }
+
     }
 
     public Optional<Traveler> findTravelerByUserId(int id) {
