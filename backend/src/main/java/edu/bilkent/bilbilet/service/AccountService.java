b97f9cf749f081fa2f991cde4c656b1a1358befc
Ilkim Elif Kervan <elif.kervan.1907@gmail.com>
2023-06-03 12:33:32 +0300

    Fix

--- backend/src/main/java/edu/bilkent/bilbilet/service/AccountService.java
+++ backend/src/main/java/edu/bilkent/bilbilet/service/AccountService.java
@@ -1,16 +1,19 @@
 package edu.bilkent.bilbilet.service;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Optional;
+import java.util.Random;
+import java.util.UUID;
+
+import edu.bilkent.bilbilet.model.*;
+import edu.bilkent.bilbilet.request.CompanyRegister;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import edu.bilkent.bilbilet.exception.AlreadyExistException;
-import edu.bilkent.bilbilet.exception.ItemNotFoundException;
-import edu.bilkent.bilbilet.exception.PasswordNotMatchException;
-import edu.bilkent.bilbilet.model.Traveler;
-import edu.bilkent.bilbilet.model.User;
+import edu.bilkent.bilbilet.enums.UserType;
 import edu.bilkent.bilbilet.repository.AccountRepository;
 import edu.bilkent.bilbilet.request.TravelerRegister;
 import edu.bilkent.bilbilet.request.UserLogin;
@@ -41,7 +44,7 @@ public class AccountService {
             User dbUser = accountRepository.findUserByMail(user.getEmail());
 
             if (dbUser == null) {
-                throw new ItemNotFoundException("User is not found");
+                throw new Exception("user is not found");
             }
 
             String hashedPassword = dbUser.getPassword();
@@ -52,10 +55,10 @@ public class AccountService {
             // boolean passwordMatch = hashedPassword.equals(dbUser.getPassword());
 
             if (!passwordMatch) {
-                throw new PasswordNotMatchException("Passwords do not match");
+                throw new Exception("passwords do not match");
             }
 
-            System.out.println("Passwords are matched");
+            System.out.println("passwords are matched");
 
             // authManager.authenticate(new
             // UsernamePasswordAuthenticationToken(user.getBilkentId(),
@@ -66,10 +69,6 @@ public class AccountService {
             final String accessToken = jwtUtils.createAccessToken(userDetails);
             final String refreshToken = jwtUtils.createRefreshToken(userDetails);
             return new RUserToken(dbUser, accessToken, refreshToken);
-        } catch (PasswordNotMatchException e) {
-            throw e;
-        } catch (ItemNotFoundException e) {
-            throw e;
         } catch (Exception e) {
             System.out.println("login exception");
             e.printStackTrace();
@@ -96,43 +95,109 @@ public class AccountService {
         }
     }
 
-    // public boolean changePassword(String auth, ChangePassword passwords) throws Exception {
-    //     try {
-    //         String username = jwtUtils.extractAccessUsername(JWTFilter.getTokenWithoutBearer(auth));
-    //         User dbUser = accountRepository.findUserByBilkentId(Long.parseLong(username));
+    public CompanyInfo getCompanyInfo(int userId) throws Exception {
+        try {
+            Optional<User> optionalUser = accountRepository.findUserById(userId);
 
-    //         if (dbUser == null) {
-    //             throw new Exception("user is not found");
-    //         }
-            
-    //         String hashedPassword = dbUser.getPassword();
-    //         // username is bilkentId in this context
-            
-            
-    //         // String hashedPassword = accountRepository.getPasswordIfUserExist(Long.parseLong(username));
-    //         boolean passwordMatch = bCryptPasswordEncoder.matches(passwords.getOldPassword(), hashedPassword);
+            if (optionalUser.isEmpty()) {
+                throw new Exception("user is not found");
+            }
 
-    //         if (!passwordMatch) {
-    //             System.out.println("passwords does not match");
-    //             throw new Exception("pasword no match");
-    //         }
+            User user = optionalUser.get();
 
-    //         System.out.println("passwords are matched");
-    //         // hash new password
-    //         String hashedNewPassword = bCryptPasswordEncoder.encode(passwords.getNewPassword());
-            
-    //         dbUser.setPassword(hashedNewPassword);
-    //         int result = accountRepository.updatePassword(hashedNewPassword, dbUser.getId());
-    //         // accountRepository.deleteById(dbUser.getId());
-    //         // accountRepository.save(dbUser);
-    //         System.out.println("result: " + result);
-    //         return true;
-            
-    //     } catch (Exception e) {
-    //         System.out.println("password change exception exception");
-    //         e.printStackTrace();
-    //         throw e;
-    //     }
+            Optional<Company> company = accountRepository.findCompanyByUserId(userId);
+
+            if (company.isEmpty()) {
+                throw new Exception("No company found");
+            }
+
+            CompanyInfo companyInfo = new CompanyInfo();
+            companyInfo.setUserId(userId);
+            companyInfo.setUser(user);
+            companyInfo.setUserType(UserType.COMPANY);
+            companyInfo.setCompany(company.get());
+
+            return companyInfo;
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw e;
+        }
+
+    }
+
+    public TravelerInfo getTravelerInfo(int userId) throws Exception {
+        try {
+            Optional<User> optionalUser = accountRepository.findUserById(userId);
+
+            if (optionalUser.isEmpty()) {
+                throw new Exception("user is not found");
+            }
+
+            User user = optionalUser.get();
+
+            Optional<Traveler> traveler = accountRepository.findTravelerByUserId(userId);
+
+            if (traveler.isEmpty()) {
+                throw new Exception("No traveler found");
+            }
+
+            TravelerInfo travelerInfo = new TravelerInfo();
+            travelerInfo.setUserId(userId);
+            travelerInfo.setUser(user);
+            travelerInfo.setUserType(UserType.TRAVELER);
+            travelerInfo.setTraveler(traveler.get());
+
+            return travelerInfo;
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw e;
+        }
+
+    }
+
+    // public boolean changePassword(String auth, ChangePassword passwords) throws
+    // Exception {
+    // try {
+    // String username =
+    // jwtUtils.extractAccessUsername(JWTFilter.getTokenWithoutBearer(auth));
+    // User dbUser =
+    // accountRepository.findUserByBilkentId(Long.parseLong(username));
+
+    // if (dbUser == null) {
+    // throw new Exception("user is not found");
+    // }
+
+    // String hashedPassword = dbUser.getPassword();
+    // // username is bilkentId in this context
+
+    // // String hashedPassword =
+    // accountRepository.getPasswordIfUserExist(Long.parseLong(username));
+    // boolean passwordMatch =
+    // bCryptPasswordEncoder.matches(passwords.getOldPassword(), hashedPassword);
+
+    // if (!passwordMatch) {
+    // System.out.println("passwords does not match");
+    // throw new Exception("pasword no match");
+    // }
+
+    // System.out.println("passwords are matched");
+    // // hash new password
+    // String hashedNewPassword =
+    // bCryptPasswordEncoder.encode(passwords.getNewPassword());
+
+    // dbUser.setPassword(hashedNewPassword);
+    // int result = accountRepository.updatePassword(hashedNewPassword,
+    // dbUser.getId());
+    // // accountRepository.deleteById(dbUser.getId());
+    // // accountRepository.save(dbUser);
+    // System.out.println("result: " + result);
+    // return true;
+
+    // } catch (Exception e) {
+    // System.out.println("password change exception exception");
+    // e.printStackTrace();
+    // throw e;
+    // }
     // }
 
     public User addUser(User user) throws Exception {
@@ -141,7 +206,7 @@ public class AccountService {
             boolean userExist = accountRepository.existsByEmail(user.getEmail());
 
             if (userExist) {
-                throw new AlreadyExistException("User already exists");
+                throw new Exception("user already exists");
             } else {
                 // generate uuid and hash password if user does not exist in the system
                 // user.setId(UUID.randomUUID());
@@ -154,8 +219,37 @@ public class AccountService {
                 // accountRepository.findUserByEmail(user.getEmail()).get().getId());
                 return newUser;
             }
-        } catch (AlreadyExistException e) {
+        } catch (Exception e) {
+            e.printStackTrace();
             throw e;
+        }
+    }
+
+    public CompanyRegister addCompany(CompanyRegister companyRegister) throws Exception {
+        try {
+            // Check if user already exists by email
+            if (accountRepository.existsByEmail(companyRegister.getUser().getEmail())) {
+                throw new Exception("User already exists");
+            }
+
+            // Check if traveler already exists by user_id
+            if (accountRepository.findCompanyByUserId(companyRegister.getUser().getUserId()).isPresent()) {
+                throw new Exception("Company already exists");
+            }
+
+            // Add user
+            User newUser = addUser(companyRegister.getUser());
+
+            Company companyToAdd = companyRegister.getCompany();
+            companyToAdd.setUser_id(newUser.getUserId());
+
+            // Add Company
+            Optional<Company> optionalCompany = accountRepository.save(companyToAdd);
+            Company newCompany = optionalCompany.isPresent() ? optionalCompany.get() : null;
+
+            // Return both added user data and added traveler data
+            CompanyRegister result = new CompanyRegister(newUser, newCompany);
+            return result;
         } catch (Exception e) {
             e.printStackTrace();
             throw e;
@@ -166,12 +260,12 @@ public class AccountService {
         try {
             // Check if user already exists by email
             if (accountRepository.existsByEmail(travelerRegister.getUser().getEmail())) {
-                throw new AlreadyExistException("User already exists");
+                throw new Exception("user already exists");
             }
 
             // Check if traveler already exists by user_id
             if (accountRepository.findTravelerByUserId(travelerRegister.getUser().getUserId()).isPresent()) {
-                throw new AlreadyExistException("Traveler already exists");
+                throw new Exception("traveler already exists");
             }
 
             // Add user
@@ -187,13 +281,125 @@ public class AccountService {
             // Return both added user data and added traveler data
             TravelerRegister result = new TravelerRegister(newUser, newTraveler);
             return result;
-        } catch (AlreadyExistException e) {
-            throw e;
         } catch (Exception e) {
             e.printStackTrace();
             throw e;
         }
     }
+
+    // @Override
+    // public RRegisterStaff addStaff(RegisterStaff staff) throws Exception {
+    // try {
+    // String generatedPassword = StudentPlacementService.generatePsw();
+
+    // User newUser = addUser(new User(UUID.randomUUID(), staff.getName(),
+    // staff.getSurname(), staff.getEmail(), staff.getBilkentId(),
+    // generatedPassword, staff.getUserType()));
+
+    // // check if staff has necessary info
+    // if (newUser.getUserType() == UserType.COORDINATOR || newUser.getUserType() ==
+    // UserType.INSTRUCTOR) {
+    // if (staff.getDepartment() == null) {
+    // throw new Exception(newUser.getUserType() + " should have a department");
+    // }
+
+    // if (staff.getFaculty() == null) {
+    // throw new Exception(newUser.getUserType() + " should have a faculty");
+    // }
+    // } else if (newUser.getUserType() == UserType.FAC_MEMBER && staff.getFaculty()
+    // == null) {
+    // throw new Exception(newUser.getUserType() + " should have a faculty");
+    // }
+
+    // // save staff to database
+    // Staff staffDb = Staff.toStaff(new StaffRequest(newUser.getId(),
+    // staff.getDepartment(), staff.getFaculty()), newUser);
+    // Staff savedStaff = staffRepository.save(staffDb);
+    // RInstructorCourseAdd responseInstructorSavedCourses = null;
+
+    // // if staff is user, add instructor course
+    // if (newUser.getUserType() == UserType.INSTRUCTOR) {
+    // if (staff.getBilkentCourseCodes() != null) {
+    // responseInstructorSavedCourses =
+    // instructorCourseService.addCourseToInstructor(new
+    // InstructorCourseAdd(newUser.getId(), staff.getBilkentCourseCodes()));
+    // } else {
+    // System.out.println("instructor has no course");
+    // }
+    // }
+
+    // // set unhashed password to user so that frontend can show it
+    // savedStaff.getUser().setPassword(generatedPassword);
+    // return new RRegisterStaff(savedStaff, responseInstructorSavedCourses);
+    // } catch (Exception e) {
+    // throw e;
+    // }
+    // }
+
+    // @Override
+    // public User addUserWithUserRequest(UserRequest userRequest) throws Exception
+    // {
+    // try {
+    // // Convert the User request to User
+    // User user = User.toUser(userRequest, null);
+    // UUID id = UUID.randomUUID();
+    // user.setId(id);
+
+    // // Generate a random password
+    // String password = new Random().ints(10, 33, 122).collect(StringBuilder::new,
+    // StringBuilder::appendCodePoint, StringBuilder::append)
+    // .toString();
+    // user.setPassword(encodePassword(password));
+    // System.out.println("In account 5");
+    // accountRepository.save(user);
+    // System.out.println("In account 6");
+    // return user;
+    // } catch (Exception e) {
+    // throw e;
+    // }
+
+    // }
+
+    // @Override
+    // public HashSet<User> addUserChunk(User[] users) throws Exception {
+    // HashSet<User> usersSet = new HashSet<>(Arrays.asList(users));
+    // HashSet<User> removedUsers = new HashSet<>();
+
+    // for (User user : usersSet) {
+    // try {
+    // boolean userExist = accountRepository.existsByBilkentId(user.getBilkentId());
+
+    // if (userExist) {
+    // usersSet.remove(user);
+    // removedUsers.add(user);
+    // } else {
+    // user.setId(UUID.randomUUID());
+    // // user.setPassword(user.getPassword());
+    // user.setPassword(encodePassword(user.getPassword()));
+    // }
+    // } catch (Exception e) {
+    // throw e;
+    // }
+    // }
+
+    // try {
+    // accountRepository.saveAll(usersSet);
+    // } catch (Exception e) {
+    // System.out.println("users not saved: " + removedUsers);
+    // throw e;
+    // }
+
+    // return usersSet;
+    // }
+
+    // @Override
+    // public List<User> getUsers() {
+    // try {
+    // return accountRepository.findAll();
+    // } catch (Exception e) {
+    // throw e;
+    // }
+    // }
 
     private String encodePassword(String plainPassword) {
         try {
