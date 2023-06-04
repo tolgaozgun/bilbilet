1bbc352cef1a9b9b50c49a0598d7ad22aaa81745
Yağız Can Aslan <86242974+can-aslan@users.noreply.github.com>
2023-06-04 11:08:07 +0300

    add tickets trigger

--- backend/src/main/resources/sql/triggers.sql
+++ backend/src/main/resources/sql/triggers.sql
@@ -44,3 +44,50 @@ BEGIN
     END IF;
 END;
 //
+
+DROP TRIGGER IF EXISTS create_tickets_based_on_fare ;
+//
+
+CREATE TRIGGER create_tickets_based_on_fare AFTER INSERT ON Fare
+FOR EACH ROW
+BEGIN
+    DECLARE config_id INT;
+    DECLARE vehicle_id INT;
+    DECLARE seat_rows INT;
+    DECLARE seat_columns INT;
+    DECLARE premium_econ_class_after INT;
+    DECLARE business_class_after INT;
+    DECLARE first_class_after INT;
+    DECLARE row_counter INT DEFAULT 1;
+    DECLARE col_counter INT DEFAULT 1;
+
+    -- Set seat config id of the company vehicle
+    SELECT seat_configuration_id INTO config_id FROM CompanyVehicle WHERE vehicle_id = NEW.vehicle_id;
+    
+    SELECT total_rows, total_columns, premium_econ_class_after, business_class_after, first_class_after
+        INTO seat_rows, seat_columns, premium_econ_class_after, business_class_after, first_class_after
+        FROM SeatConfiguration
+        WHERE seat_configuration_id = config_id;
+
+    -- Create new tickets based on the seat configuration
+    WHILE row_counter <= seat_rows DO
+        WHILE col_counter <= seat_columns DO
+            INSERT INTO Ticket (ticket_status, seat_type, fare_id, seat_row, seat_column)
+            VALUES ('AVAILABLE', 
+                    CASE
+                        WHEN row_counter <= first_class_after THEN 'FIRST_CLASS'
+                        WHEN row_counter <= business_after THEN 'BUSINESS'
+                        WHEN row_counter <= premium_econ_after THEN 'PREMIUM_ECONOMY'
+                        ELSE 'ECONOMY'
+                    END,
+                    NEW.fare_id,
+                    row_counter,
+                    col_counter);
+            SET col_counter = col_counter + 1;
+        END WHILE;
+        
+        SET col_counter = 1;
+        SET row_counter = row_counter + 1;
+    END WHILE;
+END
+//
