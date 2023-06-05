f4a1e2e161f7594ef34fca5c91c70e4857e84771
CanErsoy20 <can.ersoy@ug.bilkent.edu.tr>
2023-06-05 10:37:16 +0300

    remove "log" from exceptions

--- backend/src/main/java/edu/bilkent/bilbilet/exception/ExceptionLogger.java
+++ backend/src/main/java/edu/bilkent/bilbilet/exception/ExceptionLogger.java
@@ -2,22 +2,25 @@ package edu.bilkent.bilbilet.exception;
 
 public class ExceptionLogger {
     public static String log(Exception e) {
-        String msg = "[LOG] ";
-        msg += (e.getMessage().equals(e.getLocalizedMessage())) ? e.getLocalizedMessage() : e.getLocalizedMessage() + ": " + e.getMessage();
+        String msg = "";
+        msg += (e.getMessage().equals(e.getLocalizedMessage())) ? e.getLocalizedMessage()
+                : e.getLocalizedMessage() + ": " + e.getMessage();
         // msg += "\n";
         return msg;
     }
 
     public static String warn(Exception e) {
         String msg = "[WARNING] ";
-        msg += (e.getMessage().equals(e.getLocalizedMessage())) ? e.getLocalizedMessage() : e.getLocalizedMessage() + ": " + e.getMessage();
+        msg += (e.getMessage().equals(e.getLocalizedMessage())) ? e.getLocalizedMessage()
+                : e.getLocalizedMessage() + ": " + e.getMessage();
         // msg += "\n";
         return msg;
     }
 
     public static String error(Exception e) {
         String msg = "[ERROR] ";
-        msg += (e.getMessage().equals(e.getLocalizedMessage())) ? e.getLocalizedMessage() : e.getLocalizedMessage() + ": " + e.getMessage();
+        msg += (e.getMessage().equals(e.getLocalizedMessage())) ? e.getLocalizedMessage()
+                : e.getLocalizedMessage() + ": " + e.getMessage();
         // msg += "\n";
         return msg;
     }
